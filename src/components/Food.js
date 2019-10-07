468e851f7e30b8dfea5b16e076bb9d474f1a59a0
Rémy Kaloustian <remy.kaloustian@gmail.com>
2019-10-07 10:58:03 +0200

    Adding navbar to Food page

--- src/components/Food.js
+++ src/components/Food.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import NavbarMenu from './GeneralComponents/NavbarMenu';
 
 class Food extends React.Component {
   render() {
     return (
       <div>
+      <NavbarMenu/>
         <p>{this.props.match.params.id}</p>
         <p>{this.props.food.find(x=>x.name === this.props.match.params.id).details}</p>
       </div>
