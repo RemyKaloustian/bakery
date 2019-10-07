9e657210cfb2195bb4f537a2d9429031e035cefd
Rémy Kaloustian <remy.kaloustian@gmail.com>
2019-10-07 11:01:41 +0200

    Adding add to cart button in food page

--- src/components/Food.js
+++ src/components/Food.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import NavbarMenu from './GeneralComponents/NavbarMenu';
+import { addToCart } from '../actions/cartActions';
 
 class Food extends React.Component {
   render() {
@@ -10,6 +11,11 @@ class Food extends React.Component {
       <NavbarMenu/>
         <p>{this.props.match.params.id}</p>
         <p>{this.props.food.find(x=>x.name === this.props.match.params.id).details}</p>
+        <button 
+          onClick={()=> this.props.addToCart(this.props.match.params.id)}
+        >
+          Add to Cart
+        </button>
       </div>
     );
   }
@@ -20,5 +26,9 @@ const mapStateToProps = (state) => {
     food: state.foodReducer,
   };
 }
+
+const mapDispatchToProps = {
+  addToCart,
+};
   
-export default connect(mapStateToProps, null)(withRouter(Food));
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Food));
